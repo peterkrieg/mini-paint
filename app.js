22af63066d8b8bbff00d366a01f5edae2ce06355
Peter Krieg <pkrieg2@gmail.com>
2015-10-22 12:15:16 -0600

    did in class paint example, added random color feature

---
+++ app.js
@@ -0,0 +1,43 @@
+$(function(){
+	var color = 'white';
+	var colors = 'white green red blue yellow purple';
+
+	$('#red').on('click', function() {
+		color = 'red';
+	});
+	$('#blue').on('click', function() {
+		color = 'blue';
+	});
+	$('#green').on('click', function() {
+		color = 'green';
+	});
+	$('#yellow').on('click', function() {
+		color = 'yellow';
+	});
+	$('#white').on('click', function() {
+		color = 'white';
+	});
+	$('#rand').on('click', function(){
+		color= '#'+Math.floor(Math.random()*16777215).toString(16);
+		$('#rand span').css('background-color', color);
+	});
+
+
+	$('.box').on('click', function() {
+		$(this).addClass(color);
+		$(this).css('background-color', color);
+	});
+
+	$('.box').on('dblclick', function() {
+		$(this).removeClass(colors);
+		$(this).css('background-color', '');
+	});
+
+	$('#reset').on('click', function() {
+		$('.box').removeClass(colors);
+		$('.box').css('background-color', '');
+	});
+	
+
+
+});
